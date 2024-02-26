21a38d92659965db3d9e3e13627780c6e0f3c0a9
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-02-26 19:01:00 +0400

    class name error fixed

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 
 import uuid
-from datetime import datetime
+import datetime
 
 
 class BaseModel:
@@ -11,8 +11,8 @@ class BaseModel:
     def __init__(self):
         """Base class constructor"""
         self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Str method"""
@@ -20,12 +20,12 @@ class BaseModel:
 
     def save(self):
         """save memory"""
-        self.created_at = datetime.now()
+        self.created_at = datetime.datetime.now()
 
     def to_dict(self):
         """Dict"""
-        result = {'__class__': self.__class__.__name__}
         instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = type(self).__name__
         instance_dict['created_at'] = instance_dict['created_at'].isoformat()
         instance_dict['updated_at'] = instance_dict['updated_at'].isoformat()
         return instance_dict
