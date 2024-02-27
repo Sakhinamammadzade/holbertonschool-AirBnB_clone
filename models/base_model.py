26058ae3702f3b3349e0232ac084a05cf17e9c2a
Arzumirzeb <arzumirzeb03@gmail.com>
2024-02-27 18:13:56 +0400

    file storage

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         """Str method"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """FileStorage class"""
 
 import json
+import os
 from models.base_model import BaseModel
 
 
@@ -17,25 +18,23 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         cname = obj.__class__.__name__
-        FileStorage.__objects["{} {}".format(cname, obj.id)] = obj
+        FileStorage.__objects["{}.{}".format(cname, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         new_dict = {}
-        for k in FileStorage.__objects:
-            new_dict[k] = FileStorage.__objects[k].to_dict()
-        with open(FileStorage.__file_path, "w") as file_path:
+        for k in self.__objects:
+            new_dict[k] = self.__objects[k].to_dict()
+        with open(self.__file_path, "w") as file_path:
             json.dump(new_dict, file_path)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, "r") as file_path:
-                objdict = json.load(file_path)
-                FileStorage.__objects = {}
-                for k in objdict:
-                    FileStorage.__objects[k] = new_dict[k.split(".")[0]](**objdict[key])
+            with open(self.__file_path, "r") as file_path:
+                objdict = json.load(file_path).items()
+                for k, v in objdict:
+                    v  = eval(k.split(".")[0])(**v)
+                    self.__objects[k] = v
         except Exception:
             pass
-
-
